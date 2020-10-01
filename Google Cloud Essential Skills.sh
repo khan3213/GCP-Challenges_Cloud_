cd7c5d5f25b9d1acc708aedca8aced345d626977
Rashid Ali <72204718+rashidjatoi@users.noreply.github.com>
2020-10-01 11:38:30 -0700

    Update Google Cloud Essential Skills.sh

--- Google Cloud Essential Skills.sh
+++ Google Cloud Essential Skills.sh
@@ -15,6 +15,15 @@ gcloud compute instances create apache \
 	--tags=http-server \
 	--machine-type=n1-standard-1 \
 	--metadata=startup-script-url=gs://$PROJECT_ID/resources-install-web.sh
+	
+## Create a Cloud Function
+gcloud functions deploy safLongRunJobFunc \
+  --region us-central1 \
+  --trigger-resource gs://${PROJECT}-a \
+  --trigger-event google.storage.object.finalize \
+  --stage-bucket gs://${PROJECT}-a \
+  --source dataflow-contact-center-speech-analysis/saf-longrun-job-func \
+  --runtime nodejs10
 
 #Create firewall rule that exposes the virtual machine
 gcloud compute firewall-rules create default-allow-http --direction=INGRESS --priority=1000 --network=default --action=ALLOW --rules=tcp:80 --source-ranges=0.0.0.0/0 --target-tags=http-server
